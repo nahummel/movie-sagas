e8a71728512844d32f2ced06e1ddd92482c3f57b
Natalie Hummel <natoms225@gmail.com>
2020-05-16 22:26:05 -0500

    Back button works

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 
 class Details extends Component {
 
+    handleClick = () => {
+        this.props.history.push('/')
+    } 
+
     render() {
         return (
             <div>
                 <h1>Details</h1>
+                <button onClick={this.handleClick}>Back</button>
+                <button>Edit</button>
             </div>
         )
     }
